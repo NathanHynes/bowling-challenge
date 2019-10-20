7fc2f7e15ba4af051ce272f153f9173302d09824
Nathan Hynes <nathanmail@hotmail.co.uk>
2019-10-20 14:37:25 +0100

    added roll method along with functionality to move frame on. All tests passing

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -1,14 +1,93 @@
 describe('Bowling', function () {
-  var bowling
+  var bowling;
 
 
-    beforeEach(function() {
-      bowling = new Bowling();
+  beforeEach(function() {
+    bowling = new Bowling();
+  });
+
+  describe('roll', function () {
+
+    it('records how many pins you knocked over', function () {
+      bowling.roll(1);
+      bowling.roll(2);
+      expect(bowling.frames[1]).toEqual([1,2]);
     });
 
-    describe('.bowlingTest', function () {
-      it('returns true', function () {
-        expect(bowling.bowlingTest()).toEqual(true);
-      });
+    it("returns Invalid Roll! when roll isn't allowed", function () {
+      bowling.roll(2);
+      expect(bowling.roll(9)).toEqual('Invalid Roll!');
     });
+
+    it('increases roll count at the end of every roll', function () {
+      expect(bowling.rollCount).toEqual(0);
+      bowling.roll(2);
+      expect(bowling.rollCount).toEqual(1);
+    });
+
+    it('knocks pins down', function () {
+      bowling.roll(4);
+      expect(bowling.pins).toEqual(6);
+    });
+
+    it('only records 2 rolls per frame', function () {
+      bowling.roll(1);
+      bowling.roll(2);
+      expect(bowling.frames[1]).toEqual([1,2]);
+      bowling.roll(3);
+      bowling.roll(4);
+      expect(bowling.frames[2]).toEqual([3,4]);
+    });
+  });
+
+  describe('isNextFrame', function () {
+    it('confirms when there is a next frame', function () {
+      expect(bowling.isNextFrame()).toEqual(false);
+      bowling.roll(1);
+      bowling.roll(2);
+      expect(bowling.isNextFrame()).toEqual(true);
+    });
+  });
+
+  describe('isValidRoll', function () {
+    it('reports true if roll is valid', function () {
+      bowling.roll(1);
+      expect(bowling.isValidRoll(1)).toEqual(true);
+      expect(bowling.isValidRoll(2)).toEqual(true);
+      expect(bowling.isValidRoll(5)).toEqual(true);
+      expect(bowling.isValidRoll(9)).toEqual(true);
+    });
+
+    it('reports false if roll is invalid', function () {
+      expect(bowling.isValidRoll(11)).toEqual(false);
+      bowling.roll(2);
+      expect(bowling.isValidRoll(9)).toEqual(false);
+      expect(bowling.isValidRoll(10)).toEqual(false);
+    });
+  });
+
+  describe('nextFrame', function () {
+    it('resets pins to 10', function () {
+      bowling.roll(2);
+      bowling.roll(2);
+      bowling.nextFrame();
+      expect(bowling.pins).toEqual(10);
+    });
+
+    it('increases frameIndex', function () {
+      expect(bowling.frameIndex).toEqual(1);
+      bowling.roll(2);
+      bowling.roll(2);
+      bowling.nextFrame();
+      expect(bowling.frameIndex).toEqual(2);
+    });
+  });
+
+  describe('recentFrameScore', function () {
+    it('shows the score of the most recent frame', function () {
+      bowling.roll(2);
+      bowling.roll(3);
+      expect(bowling.recentFrameScore()).toEqual(5);
+    });
+  });
 });
--- src/Bowling.js
+++ src/Bowling.js
@@ -1,7 +1,49 @@
 function Bowling() {
-
+  this.frames = {
+    1: [],
+    2: [],
+    3: [],
+    4: [],
+    5: [],
+    6: [],
+    7: [],
+    8: [],
+    9: [],
+    10: []
+  };
+  this.rollCount = 0;
+  this.frameIndex = 1;
+  this.pins = 10;
 }
 
-Bowling.prototype.bowlingTest = function (arguments) {
-  return true;
+Bowling.prototype.roll = function(pins) {
+
+  if (this.isNextFrame()) {
+    this.nextFrame();
+  }
+
+  if (this.isValidRoll(pins)) {
+    this.frames[this.frameIndex].push(pins);
+    this.pins -= pins;
+    this.rollCount++;
+  } else {
+    return 'Invalid Roll!';
+  }
+};
+
+Bowling.prototype.isNextFrame = function() {
+  return this.rollCount % 2 === 0 && this.rollCount > 1;
+};
+
+Bowling.prototype.nextFrame = function() {
+  this.pins = 10;
+  this.frameIndex++;
+};
+
+Bowling.prototype.isValidRoll = function(pins) {
+  return this.pins - pins >= 0;
+};
+
+Bowling.prototype.recentFrameScore = function () {
+  return this.frames[this.frameIndex][0] + this.frames[this.frameIndex][1];
 };
