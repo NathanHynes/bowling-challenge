87bca9046869ecc62542a32848f99911665d7329
Nathan Hynes <nathanmail@hotmail.co.uk>
2019-12-10 19:17:20 +0000

    added functionality to interface for adding rolls to frame

--- src/interface.js
+++ src/interface.js
@@ -2,14 +2,31 @@ $(document).ready(function() {
   var bowling = new Bowling();
 
 
-  function displayTest(value) {
-    var text = value;
-    $('#comments').text(text);
-  }
+  // function displayTest(value) {
+  //   var text = value;
+  //   $('#comments').text(text);
+  // }
 
   $(".pin-button").click(function() {
-    var id = $(this).attr('id')
-    console.log('hello', id)
-    displayTest(id);
+    var id = parseInt($(this).attr('id'));
+    bowling.roll(id);
+    updateFrames(bowling.frameIndex);
+    showFrameScore(bowling.frameIndex);
   });
+
+function updateFrames(frameIndex) {
+  $('#frame' + frameIndex).text(bowling.frames[frameIndex]);
+}
+
+function showFrameScore(frameIndex) {
+  var frame = frameIndex - 1;
+  var frameScore = bowling.recentFrameScore(frameIndex);
+  if (frameIndex >= 2) {
+    var previousFrame = frameIndex - 2;
+    var previousFrameScore = bowling.recentFrameScore(frameIndex - 1);
+    $('#marker' + previousFrame).text(previousFrameScore);
+  }
+  $('#marker' + frame).text(frameScore);
+}
+
 });
